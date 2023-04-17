e2622194aa329fcd92bbfd6d9afd592fc5c3d901
Ruita <charliewachira124@gmail.com>
2023-04-17 23:53:44 +0300

    updates

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-""" import pandas as pd
-import plotly.express as px
-import streamlit as st
-
-st.set_page_config(page_title="Data Visio", page_icon=":bar_chart:", layout="wide")
-
-df = pd.read_csv(
-                 io="data.csv",
-                 engine="openpyxl",
-                 sheet_name="Sheet1",
-                 skiprows=1,
-                 usecols="A:G",
-                 nrows=100,)
-io=st.sidebar.selectbox("Select the type of graph",["Bar Chart","Line Chart","Scatter Plot","Pie Chart"])
-
-
-st.dataframe(df) """
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import matplotlib.pyplot as plt
 
 # Load the university rankings data from a CSV file
 data = pd.read_csv("2011_rankings.csv")
@@ -28,14 +11,23 @@ st.title("University Rankings")
 
 # Create a sidebar with options for different chart types
 chart_type = st.sidebar.selectbox("Select chart type", ["Bar chart", "Line graph", "Pie chart"])
+Data_table = st.sidebar.checkbox("Show data table", True)
+year_rank = st.sidebar.slider("Select year", 2011, 2023, 2011)
 
 # Display the data in the selected chart type
 if chart_type == "Bar chart":
-    st.bar_chart(data)
+    # Create bar chart
+    fig, ax = plt.subplots()
+    ax.bar(data["name"], data["scores_overall"])
+
+    # Set chart title and axes labels
+    ax.set_title("Overall rankings by university")
+    ax.set_xlabel("X Label")
+    ax.set_ylabel("Y Label")
 elif chart_type == "Line graph":
-    st.line_chart(data)
+    st.line_chart(data, )
 elif chart_type == "Pie chart":
-    st.plotly_chart(px.pie(data, values='rank', names='name'))
+    st.plotly_chart(px.pie(data, values='scores_overall', names='name'))
 
 # Display the data table
 st.write("Data table", data)
