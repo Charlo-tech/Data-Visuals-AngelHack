7aa1fa979a5f6f515b687cde444427a8c357fd93
Ruita <charliewachira124@gmail.com>
2023-04-17 23:04:09 +0300

    new additions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
+""" import pandas as pd
 import plotly.express as px
 import streamlit as st
 
@@ -14,4 +14,28 @@ df = pd.read_csv(
 io=st.sidebar.selectbox("Select the type of graph",["Bar Chart","Line Chart","Scatter Plot","Pie Chart"])
 
 
-st.dataframe(df)
+st.dataframe(df) """
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Load the university rankings data from a CSV file
+data = pd.read_csv("2011_rankings.csv")
+
+# Set the page title
+st.title("University Rankings")
+
+# Create a sidebar with options for different chart types
+chart_type = st.sidebar.selectbox("Select chart type", ["Bar chart", "Line graph", "Pie chart"])
+
+# Display the data in the selected chart type
+if chart_type == "Bar chart":
+    st.bar_chart(data)
+elif chart_type == "Line graph":
+    st.line_chart(data)
+elif chart_type == "Pie chart":
+    st.plotly_chart(px.pie(data, values='rank', names='name'))
+
+# Display the data table
+st.write("Data table", data)
