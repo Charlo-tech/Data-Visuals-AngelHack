d8be8804f21b0d77fdf27a61277dd8e0a3c593cc
Ruita <charliewachira124@gmail.com>
2023-04-19 23:56:39 +0300

    more visuals

--- main.py
+++ main.py
@@ -23,12 +23,20 @@ data_2023 = pd.read_csv("2023_rankings.csv")
 data_combo = pd.concat([data_2011, data_2012, data_2013, data_2014, data_2015, data_2016, data_2017, data_2018, data_2019, data_2020, data_2021, data_2022, data_2023], axis=0)
 
 # Set the page title
-st.title("University Rankings")
+st.title("UniRank Explorer: Discovering Global Universities through Data Visualization")
+st.write("This is a streamlit dashboard to analyze university data from 2011 to 2023")
+st. write("The data is from the Times Higher Education World University Rankings as uploaded on kaggle")
+st.write("The authors of the data are: Times Higher Education")
+st.write("The data is available at: https://www.kaggle.com/datasets/r1chardson/the-world-university-rankings-2011-2023")
+st.write("The data is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License")
+st.write("The visualisation is done by Charles Ruita and Diana Mwithalii for AngelHack April Hackathon Challenge 2023")
+st.write("Data table used for most visualisations is found below")
+st.write("click on Uni on the sidebar to view more visualisations")
 
 # Create a sidebar with options for different chart types and pages
 st.sidebar.success("view other pages above")
 
-chart_type = st.sidebar.selectbox("Select chart type", ["Bar chart", "Line graph", "Pie chart"])
+chart_type = st.sidebar.selectbox("Select chart type", ["Pie Chart", "Bar Chart"])
 Data_table = st.sidebar.checkbox("Show data table", True)
 year_rank = st.sidebar.slider("Select year", 2011, 2023, 2011)
 
--- pages/2_💹_uni.py
+++ pages/2_💹_uni.py
@@ -34,6 +34,35 @@ st.plotly_chart(fig)
 st.write(table)
 st.write("The data presented shows that all universities are up and running and none is closed as of 2023")
 
+@st.cache_data
+def load_data():
+    data = pd.read_csv("2023_rankings.csv")
+    return data
+
+df = load_data()
+
+counts = df["unaccredited"].value_counts()
+
+# Filter only false values
+counts = counts[counts.index == False]
+
+# Create pie chart
+fig = px.pie(counts, values=counts, names=counts.index,
+             title="Accredited Universities pie chart",
+             color_discrete_sequence=px.colors.qualitative.Pastel1,
+             template="seaborn")
+
+fig.update_traces(textposition="inside", textinfo="percent+label")
+
+# Create table
+table = pd.DataFrame({
+    "unaccredited": counts.index,
+    "Count of False Values": counts.values
+})
+
+st.plotly_chart(fig)
+st.write(table)
+st.write("The data presented shows that all universities are accredited as of 2023 to offer the various subjects.")
 # Distribution of universities by country 2023
 
 @st.cache_data
@@ -108,4 +137,67 @@ st.write("Top 10 Computer Science Universities in 2023:")
 st.write(ranking_table)
 st.plotly_chart(fig)
 
+@st.cache_data
+def load_data():
+    data = pd.read_csv("2023_rankings.csv")
+    return data
 
+df = load_data()
+
+# Filter data
+filtered_data = df[df["subjects_offered"].str.contains("Language", na=False)]
+
+# Count universities by location
+location_counts = filtered_data["location"].value_counts().head(10)
+
+# Create bar chart
+fig = px.bar(location_counts, x=location_counts.index, y=location_counts.values,
+             title="Top 10 Locations with Universities Offering Languages",
+             color_discrete_sequence=px.colors.qualitative.Pastel1,
+             template="seaborn")
+
+fig.update_layout(xaxis_title="Location", yaxis_title="Number of Universities")
+
+# Create table
+location_table = pd.DataFrame({
+    "Location": location_counts.index,
+    "Number of Universities": location_counts.values
+})
+
+# Display bar chart and table
+st.plotly_chart(fig)
+st.write(location_table)
+
+@st.cache_data
+def load_data():
+    data = pd.read_csv("2023_rankings.csv")
+    return data
+
+df = load_data()
+filtered_data = df[(df["subjects_offered"].str.contains("Civil Engineering", na=False))]
+
+# Sort data by ranking
+top_10 = filtered_data.sort_values(by="rank", ascending=True).head(10)
+
+# Create ranking table
+ranking_table = pd.DataFrame({
+    "Rank": top_10["rank"],
+    "University": top_10["name"],
+    "Country": top_10["location"],
+    "Score": top_10["scores_overall"]
+})
+
+# Create pie chart
+location_counts = top_10["location"].value_counts()
+fig = px.pie(location_counts, values=location_counts.values, names=location_counts.index,
+             title="Top 10 Civil Engineering Universities by Location",
+             color_discrete_sequence=px.colors.qualitative.Pastel1,
+             template="seaborn",
+             hole=0.6)
+
+fig.update_traces(textinfo="percent+label", marker=dict(line=dict(color="#FFFFFF", width=2)))
+
+# Display ranking table and pie chart
+st.write("Top 10 Civil Engineering Universities in 2023:")
+st.write(ranking_table)
+st.plotly_chart(fig)
